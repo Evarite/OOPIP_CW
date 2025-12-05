e381dc3f8fc24162da9505157adbeab073f2be73
Evarite <forestingel@gmail.com>
2025-12-05 20:35:01 +0300

    Revert "Broken"
    This reverts commit 3c664e8cd368b5ca08c82cc5b0f01d1f2c12fc41.

--- OOPIP_CW/DeliverySystemManager.cpp
+++ OOPIP_CW/DeliverySystemManager.cpp
@@ -387,13 +387,12 @@ namespace DeliverySystem
 
 		while (true)
 		{
-			std::cout << "Спіс даступных грузаў:\n";
 			int i = 0;
 			for (auto& cargo : cargos)
 			{
 				if (cargo.GetCurrentDelivery() == nullptr)
 				{
-					std::cout << ++i << ". " << cargo << "\n\n";
+					std::cout << ++i << ". " << cargo << std::endl;
 					availableCargos.push_back(&cargo);
 				}
 			}
@@ -404,12 +403,12 @@ namespace DeliverySystem
 			}
 			else
 			{
-				int choiceCargo = GetIntWithinRange(1, i, "Выбярыце груз: ");
+				int choiceCargo = GetIntWithinRange(0, i, "Выбярыце груз: ");
 
 				while (true)
 				{
 					int j = 0;
-					std::cout << "\nСпіс даступных гарадоў:\n";
+
 					std::cout << std::endl << std::endl;
 					for (auto& country : countries)
 					{
@@ -426,9 +425,9 @@ namespace DeliverySystem
 					}
 					else
 					{
-						int choiceCity = GetIntWithinRange(1, j, "Выбярыце горад: ");
+						int choiceCity = GetIntWithinRange(0, j, "Выбярыце горад: ");
 
-						availableCargos[choiceCargo - 1]->RequestDelivery(account, availableCities[choiceCity - 1]);
+						availableCargos[i - 1]->RequestDelivery(account, availableCities[j - 1]);
 
 						std::cout << "Заказ сфарміраваны. Дастаўка пачнецца ў бліжайшы час." << std::endl;
 
